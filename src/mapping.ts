d9a2b9c39ed2841e6eb9192f3e4014b15a7efaea
Keiron <keiron.gulrajani@gmail.com>
2023-01-06 11:41:20 +0100

    Added overall tally of pixels and paint burned for MurAll

--- src/mapping.ts
+++ src/mapping.ts
@@ -8,12 +8,15 @@ import {
   RoleRevoked,
   SetPixelsCall
 } from "../generated/MurAll/MurAll"
-import { ArtistInfo, PaintedEventInfo } from "../generated/schema"
+import { ArtistInfo, PaintedEventInfo, MurAllInfo } from "../generated/schema"
 import { BIGDECIMAL_TWO, BIGDECIMAL_ZERO, BIGINT_TWO, BIGINT_ZERO, PAINT_TOKEN_ADDRESS_ETH, PAINT_TOKEN_ADDRESS_POLYGON, toDp, ZERO_ADDRESS } from "./constants"
 
 
 
 export function handlePainted(event: Painted): void {
+  // get the paint token address (change based on network)
+  let paintTokenAddress = PAINT_TOKEN_ADDRESS_POLYGON
+
   let receipt = event.receipt
   if(receipt == null) {
     log.error("receipt is null", [])
@@ -26,12 +29,13 @@ export function handlePainted(event: Painted): void {
     if (txLog != null && txLog.topics[0] == crypto.keccak256(ByteArray.fromUTF8("Transfer(address,address,uint256)"))) {
       let to = Address.fromString("0x"+txLog.topics[2].toHexString().slice(26))
       
-      if(to == ZERO_ADDRESS && txLog.address == PAINT_TOKEN_ADDRESS_POLYGON) {
+      if(to == ZERO_ADDRESS && txLog.address == paintTokenAddress) {
         // burn event
         log.warning("Burn event {}", [event.transaction.hash.toHexString()])
 
         let from = Address.fromString("0x"+txLog.topics[1].toHexString().slice(26)) as Address
         let artistInfo = getArtistInfo(from)
+        let murAllInfo = getMurAllInfo()
         
         let paintBurned = BigInt.fromUnsignedBytes(Bytes.fromUint8Array(txLog.data.reverse())).toBigDecimal()
         let paintBurnedDp = toDp(paintBurned, 18)
@@ -39,6 +43,11 @@ export function handlePainted(event: Painted): void {
         artistInfo.totalPaintBurned = artistInfo.totalPaintBurned.plus(paintBurned)
         artistInfo.totalPaintBurnedCorrectDp = artistInfo.totalPaintBurnedCorrectDp.plus(paintBurnedDp)
         artistInfo.save()
+        
+        murAllInfo.totalPixelsDrawn = murAllInfo.totalPixelsDrawn.plus(paintBurnedDp.times(BIGDECIMAL_TWO))
+        murAllInfo.totalPaintBurned = murAllInfo.totalPaintBurned.plus(paintBurned)
+        murAllInfo.totalPaintBurnedCorrectDp = murAllInfo.totalPaintBurnedCorrectDp.plus(paintBurnedDp)
+        murAllInfo.save()
 
         let paintedEventInfo = new PaintedEventInfo(event.transaction.hash.toHexString())
         paintedEventInfo.artist = from
@@ -63,4 +72,17 @@ function getArtistInfo (address: Address): ArtistInfo {
     artistInfo.save()
   }
   return artistInfo
+}
+
+function getMurAllInfo (): MurAllInfo {
+  let address = Address.fromString("0x0000000000000000000000000000000000000000")
+  let murAllInfo = MurAllInfo.load(address.toHexString())
+  if (murAllInfo == null) {
+    murAllInfo = new MurAllInfo(address.toHexString())
+    murAllInfo.totalPaintBurned = BIGDECIMAL_ZERO
+    murAllInfo.totalPaintBurnedCorrectDp = BIGDECIMAL_ZERO
+    murAllInfo.totalPixelsDrawn = BIGDECIMAL_ZERO
+    murAllInfo.save()
+  }
+  return murAllInfo
 }
